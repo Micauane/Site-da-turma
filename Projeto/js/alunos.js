5a48164a82916d40239eab281e20bd620437c4a4
Luan Guimarães <luan.guimaraes@live.com>
2018-03-28 15:38:11 -0300

    Up

--- Projeto/js/alunos.js
+++ Projeto/js/alunos.js
@@ -15,28 +15,39 @@
     myImage3.setAttribute("src", imageArray[imageDep]);
 
     if(imageIndex == 0){
+      document.getElementById("demo3").innerHTML = "Viviele";
       document.getElementById("demo").innerHTML = "Micauane";
+      document.getElementById("demo2").innerHTML = "Weslany";
 
     }
     else if (imageIndex == 1) {
+      document.getElementById("demo3").innerHTML = "Micauane";
       document.getElementById("demo").innerHTML = "Weslany";
+      document.getElementById("demo2").innerHTML = "Gustavo";
 
     }
     else if (imageIndex == 2) {
+      document.getElementById("demo3").innerHTML = "Weslany";
       document.getElementById("demo").innerHTML = "Gustavo";
+      document.getElementById("demo2").innerHTML = "Edwardes";
 
     }
     else if (imageIndex == 3) {
+      document.getElementById("demo3").innerHTML = "Gustavo";
       document.getElementById("demo").innerHTML = "Edwardes";
+      document.getElementById("demo2").innerHTML = "Romulo";
 
     }
     else if (imageIndex == 4) {
+      document.getElementById("demo3").innerHTML = "Edwardes";
       document.getElementById("demo").innerHTML = "Romulo";
+      document.getElementById("demo2").innerHTML = "Viviele";
 
     }
     else if (imageIndex == 5) {
+      document.getElementById("demo3").innerHTML = "Romulo";
       document.getElementById("demo").innerHTML = "Viviele";
-
+      document.getElementById("demo2").innerHTML = "Micauane";
     }
 
 
